ea08dda49e3fa56b21b1b478d972d1df84e42ade
Vlad Kolomysov <kolomys@gmail.com>
2017-03-29 22:01:55 +0300

    feat: build it bigger usacity project

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/OnJokeReceivedListener.java
@@ -0,0 +1,10 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by vlad_kolomysov on 28/03/17.
+ */
+
+public interface OnJokeReceivedListener {
+
+    void onReceived(String joke);
+}
